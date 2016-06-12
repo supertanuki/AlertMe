5138f487b51fcc9be257af6acf822f2ae4b12278
Richard Hanna <richard.hanna@elao.com>
2016-06-12 14:51:28 +0200

    Downgrade dependencies; Remove useless dependencies

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -13,11 +13,8 @@ class AppKernel extends Kernel
             new Symfony\Bundle\TwigBundle\TwigBundle(),
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
 
-            // Sonata - Intl
-            new Sonata\IntlBundle\SonataIntlBundle(),
             new AppBundle\AppBundle(),
         );
 
@@ -26,9 +23,6 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
-
-            // Fixtures
-            $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
         }
 
         return $bundles;
